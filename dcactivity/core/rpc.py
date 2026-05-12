61622dee17ca68944b59bb29e60777d34e905664
Mik0-T3ch <chrispino2109@gmail.com>
2026-05-12 00:39:24 -0600

    el rpc

---
+++ dcactivity/core/rpc.py
@@ -0,0 +1,39 @@
+import time
+
+from pypresence import Presence
+
+CLIENT_ID = "1446336643320647720"
+
+
+class DiscordRPC:
+    def __init__(self):
+        self.rpc = Presence(CLIENT_ID)
+        self.connected = False
+
+    def connect(self):
+        if self.connected:
+            return
+
+        try:
+            self.rpc.connect()
+            self.connected = True
+
+            print("[RPC] Connected to Discord")
+
+        except Exception as e:
+            print(f"[RPC ERROR] {e}")
+
+    def update(self, state: str, elapsed: int):
+        try:
+            self.rpc.update(
+                details="DcActivity",
+                state=state,
+                large_image="shell",
+                large_text="Linux Activity",
+                start=int(time.time()) - elapsed
+            )
+
+        except Exception as e:
+            print(f"[RPC UPDATE ERROR] {e}")
+
+            self.connected = False
